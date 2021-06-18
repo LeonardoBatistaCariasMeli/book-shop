c49dcfcd1c42204edbe8c958e91c98fe13151369
LeonardoBatistaCariasMeli <leonardo.carias@mercadolivre.com>
2021-06-18 14:55:58 -0300

    First commit

---
+++ src/test/java/com/digitalhouse/bookshop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
